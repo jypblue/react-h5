5839d27b50b17cf6cae2434107916072f33c5231
wangzhaoxiong <wangzhaoxiong@yit.com>
2018-09-28 10:58:54 +0800

    add redux-devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,23 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import rootReducer from './reducers';
 
 const loggerMiddleware = createLogger();
 
 export default function configureStore(preloadedState) {
-  return createStore(
-    rootReducer,
-    preloadedState,
-    applyMiddleware(
-      thunkMiddleware,
-      loggerMiddleware
-    )
-  );
+  const createStoreWithMiddleware = composeWithDevTools(applyMiddleware(thunkMiddleware,
+    loggerMiddleware))(createStore);
+  return createStoreWithMiddleware(rootReducer,
+    preloadedState);
+  // return createStore(
+  //   rootReducer,
+  //   preloadedState,
+  //   applyMiddleware(
+  //     thunkMiddleware,
+  //     loggerMiddleware
+  //   )
+  // );
 }
--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+
 class Login extends Component {
   constructor(props) {
     super(props);
